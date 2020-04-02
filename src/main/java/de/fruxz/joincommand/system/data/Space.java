3f2bada423711d3876712d8e379dab1da1e40063
Fruxz <cedricspitzer@outlook.de>
2020-04-02 16:38:43 +0200

    Added Space class
    Added Space class, that contains preferences

---
+++ src/main/java/de/fruxz/joincommand/system/data/Space.java
@@ -0,0 +1,13 @@
+package de.fruxz.joincommand.system.data;
+
+import de.fruxz.joincommand.system.configuration.Preference;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Space {
+
+    public static Preference<List<String>> list_joinCommands = new Preference<>("system.joinCommands", Collections.emptyList());
+    public static Preference<List<String>> list_firstJoinCommands = new Preference<>("system.fistJoinCommands", Collections.emptyList());
+
+}
